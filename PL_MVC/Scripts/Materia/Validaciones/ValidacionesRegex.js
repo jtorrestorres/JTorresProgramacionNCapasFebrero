939972d93c92ab6c662bf98bbe2c419f58b38011
JorgeGF <jguevaraflores3@gmail.com>
2025-04-29 17:42:28 -0600

    validacion CURP

--- PL_MVC/Scripts/Materia/Validaciones/ValidacionesRegex.js
+++ PL_MVC/Scripts/Materia/Validaciones/ValidacionesRegex.js
@@ -33,7 +33,37 @@ function ValidarSoloLetras(event) {
         return false;
     }
     }
-    
+
+
+function ValidarEmail(input) {
+    console.log(input.value)
+    var pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+
+    if (pattern.test(input.value)) {
+        //input se coloree de verde
+        input.style.border = "5px solid green";
+    } else {
+        //mandar un mensaje de error
+        //input rojo
+        $("#txtErrorMessage").text("Error")
+        alert('correo erroneo')
+    }
+}
+
+function ValidarCURP(input) {
+    var regex = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0\d|1[0-2])(?:[0-2]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/;
+
+    if (regex.test(input.value)) {
+        //let span = $(input).closest("div").find("span");
+        $(input).css('border-color', 'green')    
+        $(input).css('border-width', '3px')    
+        let span = $(input).siblings("span");
+        $(span[0]).text('El CURP es correcto').css("color", 'green')
+        alert('es correcto')
+    } else {
+        alert('es incorrecto')
+    }
+}
 
     
 
